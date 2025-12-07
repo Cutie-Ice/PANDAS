9ffa233bfd8567d2b39437055bf0a2147ac52482
Ibenne Chibuzo Emmanuel <chibuziemmanuel7@gmail.com>
2025-12-07 13:38:30 +0100

    Update project files and remove temporary data files

--- New folder/on_csv.py
+++ New folder/on_csv.py
@@ -1,190 +1,191 @@
 import csv
+import pandas as pd
+import os
 
-# with open('customers.csv', mode='r') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         print(row)
+with open('customers.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
 
-# with open('access_logs.csv', mode='r') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         print(row)
+with open('access_logs.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
 
 
-# with open('students.csv', 'r') as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         print(f"Name: {row['Name']}, Score: {row['Score']}")
-        # print(row)
+with open('students.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(f"Name: {row['Name']}, Score: {row['Score']}")
+        print(row)
 
         
-# with open('students.csv', mode='r') as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         if int(row['Score']) > 80:
-#             print(f"{row['Name']} passed with {row['Score']}")
+with open('students.csv', mode='r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if int(row['Score']) > 80:
+            print(f"{row['Name']} passed with {row['Score']}")
 
-# data = [['Name', 'Score'], ['Ada', 88], ['Bola', 76]]
+data = [['Name', 'Score'], ['Ada', 88], ['Bola', 76]]
 
-# with open('output.csv', mode='w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerows(data)
-# # import csv
+with open('output.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+# import csv
 
-# students = [
-#     {'Name': 'Ada', 'Subject': 'Math', 'Score': 88},
-#     {'Name': 'Bola', 'Subject': 'Science', 'Score': 76},
-#     {'Name': 'Chidi', 'Subject': 'English', 'Score': 92}
-# ]
+students = [
+    {'Name': 'Ada', 'Subject': 'Math', 'Score': 88},
+    {'Name': 'Bola', 'Subject': 'Science', 'Score': 76},
+    {'Name': 'Chidi', 'Subject': 'English', 'Score': 92}
+]
 
-# with open('students_output.csv', mode='w', newline='') as file:
-#     fieldnames = ['Name', 'Subject', 'Score']
-#     writer = csv.DictWriter(file, fieldnames=fieldnames)
+with open('students_output.csv', mode='w', newline='') as file:
+    fieldnames = ['Name', 'Subject', 'Score']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
     
-#     writer.writeheader()  # Write the header row
-#     writer.writerows(students)
+    writer.writeheader()  # Write the header row
+    writer.writerows(students)
 
-# profiles = [
-#     {'First Name': 'Chibuzo', 'Last Name': 'Emmanuel', 'D.O.B': '14 Oct. 1994', 'Course': "Data Engr."},
-#     {'First Name': 'Happines', 'Last Name': 'Okafor', 'D.O.B': '12 March 1999', 'Course': "AI & Prompt Engr."},
-#     {'First Name': 'Esther', 'Last Name': 'Dike', 'D.O.B': '14 April 2002', 'Course': "Data Engr."}
-# ] 
+profiles = [
+    {'First Name': 'Chibuzo', 'Last Name': 'Emmanuel', 'D.O.B': '14 Oct. 1994', 'Course': "Data Engr."},
+    {'First Name': 'Happines', 'Last Name': 'Okafor', 'D.O.B': '12 March 1999', 'Course': "AI & Prompt Engr."},
+    {'First Name': 'Esther', 'Last Name': 'Dike', 'D.O.B': '14 April 2002', 'Course': "Data Engr."}
+] 
 
-# with open("profile.csv", mode='w', newline="") as file:
-#     fieldnames = ['First Name', 'Last Name', 'D.O.B', 'Course']
-#     writer = csv.DictWriter(file, fieldnames=fieldnames)
+with open("profile.csv", mode='w', newline="") as file:
+    fieldnames = ['First Name', 'Last Name', 'D.O.B', 'Course']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-#     writer.writeheader()
-#     writer.writerows(profiles) 
+    writer.writeheader()
+    writer.writerows(profiles) 
 
-# with open('profile.csv', mode= 'r') as file:
-#     reader = csv.DictReader(file)
-#     next(reader)
-#     for row in reader:
-#         #print(f"First Name:{row['First Name']} Last Name:{row['Last Name']} D.O.B:{row['D.O.B']} Course:{row['Course']}")
-#         print(row)
+with open('profile.csv', mode= 'r') as file:
+    reader = csv.DictReader(file)
+    next(reader)
+    for row in reader:
+        print(f"First Name:{row['First Name']} Last Name:{row['Last Name']} D.O.B:{row['D.O.B']} Course:{row['Course']}")
+        print(row)
 
-# new_profiles = [
-#     ['David', 'Elizerbirth', '14 Oct. 1997', "Edu. Chemistry"],
-#     ['Mba', 'Jennifer', '13 March 1997', "HIR"],
-#     ['Christopher',  'Chima',  '14 April 1995',  "Accounting"]
-# ] 
+new_profiles = [
+    ['David', 'Elizerbirth', '14 Oct. 1997', "Edu. Chemistry"],
+    ['Mba', 'Jennifer', '13 March 1997', "HIR"],
+    ['Christopher',  'Chima',  '14 April 1995',  "Accounting"]
+] 
 
-# with open('profile.csv', '+a', newline='\n', encoding='utf-8')  as file:
-#     writer= csv.writer(file)
-#     writer.writerows(new_profiles)
-#     print("appending successful")
+with open('profile.csv', '+a', newline='\n', encoding='utf-8')  as file:
+    writer= csv.writer(file)
+    writer.writerows(new_profiles)
+    print("appending successful")
 
 data = [
     {1, "Admin", "Mechanical"}, 
     {2, "CEO", 'Electrical'},
     ]
 
-# with open('csv_file.csv', '+a', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerows(data)
-#     print('Appended')
+with open('csv_file.csv', '+a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+    print('Appended')
 
 
-# new_data = {1, 'Snr. Pro.', 'IT'}
+new_data = {1, 'Snr. Pro.', 'IT'}
     
-# with open('csv_file.csv', 'a',newline='') as file:
-#     writer = csv.writer(file, delimiter= "|")
-#     writer.writerows(new_data)
-#     print('Another one')
-
-import pandas as pd
-import os
-
-
-# try:
-#     df = pd.read_csv("profile.csv")
-#     print('Successfully read data.csv')
-# except FileNotFoundError:
-#     print('Error: The file does not exist!')
-
-# df_new = pd.DataFrame({'A': [1, 2], 'B': ['x', 'y']})
-# df_new.to_csv('new_data.csv', index=False, mode='w')
-
-# df_append = pd.DataFrame({'A': [3], 'B': ['z']})
-# df_append.to_csv('new_data.csv', index=False, mode='a', header= False)
-
-# df_sc = pd.read_csv('semi_colon.csv', sep=';')
-
-# print('---DataFrame with Semicolon Delimiter ---')
-# print(df_sc)
-
-# df_append = pd.DataFrame({'ID': [101], 'Name': 'Femi', "Score": [90.2]})
-# df_append.to_csv('semi_colon.csv', index=False, mode='a', header= False, sep=';')
-
-# df_read = pd.read_csv("semi_colon.csv",)
-# print(df_read)
-# print("Successfully Read!!!")
-
-# df_missing = pd.DataFrame({
-#     'Temp_C': [25, 28, None, 22, 26],
-#     'City': ['London', 'Paris', 'Berlin', 'Rome', None]
-# })
-
-# df_dropped = df_missing.dropna()
-# print(df_dropped)
-
-# df_reading = pd.read_csv('semi_colon.csv')
-# read = df_reading.dropna()
-# print(read)
+with open('csv_file.csv', 'a',newline='') as file:
+    writer = csv.writer(file, delimiter= "|")
+    writer.writerows(new_data)
+    print('Another one')
 
 
 
-# x = range(0,100,2)
-# print(tuple(x))
-# z= []
-# x= range(0,100,2)
-# for i in x:
-#     z.append(i)
-#     print(z)
 
-# x = 2
-# while x < 100:
-#     print(x)
-#     x += 2
-# from openpyxl.workbook import workbook
+try:
+    df = pd.read_csv("profile.csv")
+    print('Successfully read data.csv')
+except FileNotFoundError:
+    print('Error: The file does not exist!')
 
-# df_convert = pd.DataFrame({
-#     'Item': ['Apple', 'Banana'],
-#     'Price': [1.00, 0.50]
-# })
+df_new = pd.DataFrame({'A': [1, 2], 'B': ['x', 'y']})
+df_new.to_csv('new_data.csv', index=False, mode='w')
 
-# data = df_convert.to_json(orient='records', indent=4)
-# print(data)
+df_append = pd.DataFrame({'A': [3], 'B': ['z']})
+df_append.to_csv('new_data.csv', index=False, mode='a', header= False)
 
-# data = df_convert.to_excel('data.xlsx', sheet_name='Products', index=False)
+df_sc = pd.read_csv('semi_colon.csv', sep=';')
+
+print('---DataFrame with Semicolon Delimiter ---')
+print(df_sc)
+
+df_append = pd.DataFrame({'ID': [101], 'Name': 'Femi', "Score": [90.2]})
+df_append.to_csv('semi_colon.csv', index=False, mode='a', header= False, sep=';')
+
+df_read = pd.read_csv("semi_colon.csv",)
+print(df_read)
+print("Successfully Read!!!")
+
+df_missing = pd.DataFrame({
+    'Temp_C': [25, 28, None, 22, 26],
+    'City': ['London', 'Paris', 'Berlin', 'Rome', None]
+})
+
+df_dropped = df_missing.dropna()
+print(df_dropped)
+
+df_reading = pd.read_csv('semi_colon.csv')
+read = df_reading.dropna()
+print(read)
+
+
+
+x = range(0,100,2)
+print(tuple(x))
+z= []
+x= range(0,100,2)
+for i in x:
+    z.append(i)
+    print(z)
+
+x = 2
+while x < 100:
+    print(x)
+    x += 2
+from openpyxl.workbook import workbook
+
+df_convert = pd.DataFrame({
+    'Item': ['Apple', 'Banana'],
+    'Price': [1.00, 0.50]
+})
+
+data = df_convert.to_json(orient='records', indent=4)
+print(data)
+
+data = df_convert.to_excel('data.xlsx', sheet_name='Products', index=False)
 
 import csv
-# import json
+import json
 
-# csv_file = 'students.csv'
-# json_file = 'students.json'
+csv_file = 'students.csv'
+json_file = 'students.json'
 
-# data = []
-# with open(csv_file, mode='r') as cf:
-#     reader = csv.DictReader(cf)
-#     for row in reader:
-#         data.append(row)
+data = []
+with open(csv_file, mode='r') as cf:
+    reader = csv.DictReader(cf)
+    for row in reader:
+        data.append(row)
 
-# with open(json_file, mode='w') as jf:
-#     json.dump(data, jf, indent= 4)
+with open(json_file, mode='w') as jf:
+    json.dump(data, jf, indent= 4)
 
-# with open('students.csv', mode='r') as cf:
-#     rd = csv.DictReader(cf)
-#     students = list(rd)
-#     print(students)
+with open('students.csv', mode='r') as cf:
+    rd = csv.DictReader(cf)
+    students = list(rd)
+    print(students)
 
-# with open("students.csv", mode="r") as file:
-#     reader = csv.DictReader(file,)
+with open("students.csv", mode="r") as file:
+    reader = csv.DictReader(file,)
 
-#     for row in reader:
-#         if int(row["Score"]) > 80:
-#             print(row)
+    for row in reader:
+        if int(row["Score"]) > 80:
+            print(row)
 
 
 with open('students.csv', mode="r") as file:
--- New folder/on_json.py
+++ New folder/on_json.py
@@ -1,11 +1,11 @@
 import json
 
 
-# with open('student.json', 'r') as file:
-#     data = json.load(file)
-#     print(data['name'])
-#     print(data['age'])
-#     print(data['courses'])
+with open('student.json', 'r') as file:
+    data = json.load(file)
+    print(data['name'])
+    print(data['age'])
+    print(data['courses'])
 
 
 import json
@@ -16,39 +16,39 @@ student_data = {
     "courses": ["History", "Literature"]
 }
 
-# with open('student.json', 'w') as file:
-#     json.dump(student_data, file, indent=4)
-
-import json
-
-# student = {"name": "Ada", "age": 25}
-# json_string = json.dumps(student, indent=2)
-
-# print(json_string)
+with open('student.json', 'w') as file:
+    json.dump(student_data, file, indent=4)
 
 # import json
 
-# data = '''
-# {
-#   "user": {
-#     "id": 1,
-#     "name": "John Doe",
-#     "details": {
-#       "age": 28,
-#       "location": "USA",
-#       "hobbies": ["reading", "cycling"]
-#     }
-#   }
-# }
-# '''
+student = {"name": "Ada", "age": 25}
+json_string = json.dumps(student, indent=2)
 
-# parsed = json.loads(data)
-# print(parsed["user"]["name"])  # Output: John Doe
-# print(parsed["user"]["details"]["age"])  # Output: 28
-# print(parsed["user"]["details"]["hobbies"][1][::-1])
-# print(parsed["user"]["details"]["location"])
+print(json_string)
 
 import json
+
+data = '''
+{
+  "user": {
+    "id": 1,
+    "name": "John Doe",
+    "details": {
+      "age": 28,
+      "location": "USA",
+      "hobbies": ["reading", "cycling"]
+    }
+  }
+}
+# '''
+
+parsed = json.loads(data)
+print(parsed["user"]["name"])  # Output: John Doe
+print(parsed["user"]["details"]["age"])  # Output: 28
+print(parsed["user"]["details"]["hobbies"][1][::-1])
+print(parsed["user"]["details"]["location"])
+
+# import json
 
 data = '''
 {
@@ -60,88 +60,88 @@ data = '''
 }
 '''
 
-# parsed = json.loads(data)
-# for student in parsed["students"]:
-#     print(f"{student['name']}: {student['score']}")
+parsed = json.loads(data)
+for student in parsed["students"]:
+    print(f"{student['name']}: {student['score']}")
 
-# data1 = {
-#     "employee": {
-#         "name": "John",
-#         "department": {"name": "Engineering", "floor": 3},
-#         "address": {"city": "Lagos", "country": "Nigeria"}
-#     }
-# }
+data1 = {
+    "employee": {
+        "name": "John",
+        "department": {"name": "Engineering", "floor": 3},
+        "address": {"city": "Lagos", "country": "Nigeria"}
+    }
+}
 
-# flat_data = {
-#     "name": data1["employee"]["name"],
-#     "department_name": data1["employee"]["department"]["name"],
-#     "department_floor": data1["employee"]["department"]["floor"],
-#     "city": data1["employee"]["address"]["city"],
-#     "country": data1["employee"]["address"]["country"]
-# }
+flat_data = {
+    "name": data1["employee"]["name"],
+    "department_name": data1["employee"]["department"]["name"],
+    "department_floor": data1["employee"]["department"]["floor"],
+    "city": data1["employee"]["address"]["city"],
+    "country": data1["employee"]["address"]["country"]
+}
 
-# print(flat_data)
+print(flat_data)
 
-# data2 = {
-#     "Id": 1,
-#     "name": "John",
-#     "address": {
-#         "street": "123 Main St",
-#         "city": "New York",
-#         "state": "NY"
-#     },
-#     "contacts": [
-#         {"type": "email", "value": "john@example.com"},
-#         {"type": "phone", "value": "123-456-7890"}
-#     ]
-# }
-# flat_data2 = {
-#     "id": data2["Id"],
-#     "name": data2["name"],
-#     "add_street": data2["address"]["street"],
-#     "add_city": data2["address"]["city"],
-#     "add_state": data2["address"]["state"],
-#     "con_type": data2["contacts"][0]["type"],
-#     "con_value": data2["contacts"][0]["value"],
-#     "con_type2": data2["contacts"][-1]["type"], 
-#     "con_value2": data2["contacts"][-1]["value"]
-#  }
-# print(flat_data2)
-
-
-# import json
-# import csv
-
-# with open('students.json', 'r') as json_file:
-#     data = json.load(json_file)
-
-# with open('students.csv', 'w', newline='') as csv_file:
-#     if data:
-#         fieldnames = data[0].keys()
-#         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerows(data)
+data2 = {
+    "Id": 1,
+    "name": "John",
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "state": "NY"
+    },
+    "contacts": [
+        {"type": "email", "value": "john@example.com"},
+        {"type": "phone", "value": "123-456-7890"}
+    ]
+}
+flat_data2 = {
+    "id": data2["Id"],
+    "name": data2["name"],
+    "add_street": data2["address"]["street"],
+    "add_city": data2["address"]["city"],
+    "add_state": data2["address"]["state"],
+    "con_type": data2["contacts"][0]["type"],
+    "con_value": data2["contacts"][0]["value"],
+    "con_type2": data2["contacts"][-1]["type"], 
+    "con_value2": data2["contacts"][-1]["value"]
+ }
+print(flat_data2)
 
 
 import json
+import csv
 
-# with open("students.json", 'r') as file:
-#     loading = json.load(file)
-#     print(loading)
-#     with open('new_stud.json', 'w', newline='') as file:
-#         json.dump(loading, file, indent=2, sort_keys=True)
+with open('students.json', 'r') as json_file:
+    data = json.load(json_file)
 
-#     print("Data written to file!")
+with open('students.csv', 'w', newline='') as csv_file:
+    if data:
+        fieldnames = data[0].keys()
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+# import json
+
+with open("students.json", 'r') as file:
+    loading = json.load(file)
+    print(loading)
+    with open('new_stud.json', 'w', newline='') as file:
+        json.dump(loading, file, indent=2, sort_keys=True)
+
+    print("Data written to file!")
 
     
-# import json
-# from urllib.request import urlopen
+import json
+from urllib.request import urlopen
 
-# with urlopen("") as response:
-#     source = response.read()
+with urlopen("") as response:
+    source = response.read()
 
-# data = json.loads(source)
-# print(data)
+data = json.loads(source)
+print(data)
 
 class_b = ["Dalington", "Emeka", 'Cynthia']
 joining = ' '.join(class_b)
